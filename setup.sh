ef2b1475ef320e3900fcb988d45d9ceb5dcb350d
Jacob Duijzer <jacob@duijzer.com>
2020-07-16 22:01:22 +0100

    Added zsh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,16 @@
 
 apt-get install git zsh zsh-autosuggestions autojump \
 	ttf-ubuntu-font-family ttf-dejavu
+
+
+echo "Setup zsh"
+mkdir ~/.zsh
+chsh -s $(which zsh)
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+if [ -f ~/.zshrc ]; then
+	mv ~/.zshrc ~/.zshrc.bak
+fi
+
+ln -fs ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -fs ~/.dotfiles/zsh/antigen.zsh ~/.antigen.zsh
+ln -fs ~/.dotfiles/zsh/aliases.zsh ~/.aliases.zsh
---
+++ zsh/aliases.zsh
@@ -0,0 +1,4 @@
+# general
+alias vi="vim"
+alias g=git
+alias k=sudo k3s kubectl
